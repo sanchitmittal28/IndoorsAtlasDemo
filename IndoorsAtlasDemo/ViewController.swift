bd1b10979dbf3fa26f19b127392b32331c691c12
Sanchit Mittal <sanchit.mitttal@hotcocoasfotware.com>
2018-07-02 17:03:34 +0530

    Getting location and floor

--- IndoorsAtlasDemo/ViewController.swift
+++ IndoorsAtlasDemo/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import IndoorAtlas
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, IALocationManagerDelegate {
+    
+    var locationManager: IALocationManager = IALocationManager.sharedInstance()
 
+    let kAPIKey = "660e9157-3c4c-41dd-872d-68c9729ba3bd"
+    let kAPISecret = "yuy/dWyMTboHe4QUEMXEEKIh+KIXEzj5ArBOT7VlQPtSrAwq9EHCUVVy+VpKtsSPYakf9Llt+a/55oGqBbxc92j4RORGH9ewWqvv/SfAKWCl3i5fw32sWaBP0l24XQ=="
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.setApiKey(kAPIKey, andSecret: kAPISecret)
+        
+        locationManager.startUpdatingLocation()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func indoorLocationManager(_ manager: IALocationManager, didUpdateLocations locations: [Any]) {
+        
+        let l = locations.last as! IALocation
+        
+        if let newLocation = l.location?.coordinate {
+            print("Position changed to coordinate: \(newLocation.latitude) \(newLocation.longitude)")
+        }
     }
-
-
 }
 
---
+++ Pods/Target Support Files/Pods-IndoorsAtlasDemo/Pods-IndoorsAtlasDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IndoorsAtlasDemo : NSObject
+@end
+@implementation PodsDummy_Pods_IndoorsAtlasDemo
+@end
